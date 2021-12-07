664a8fcfac55f8ab615508e5b80ca766490006a7
calebdkofahl <95653486+calebdkofahl@users.noreply.github.com>
2021-12-07 00:10:12 -0500

    Add files via upload
    Committing updated notebook, data files, and images from the simulations. Still more to be done.

---
+++ src/Depricated/scratch.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+import simpy
+import pandas as pd
+import numpy as np
+
+data_path = "./data/squeued.20211111.mergedparts2.csv"
+sdata = pd.read_csv(data_path)
+sdata = sdata.sort_values(by="SUBMIT_TIME")
+l = pd.to_datetime(sdata["SUBMIT_TIME"]).sort_values().diff().dropna()
+
+print(l[l > timedelta(hours=100)])
+
+l = np.array([])
+l.put
+
+
+'''TODO:
+    1) See what raising the number of nodes does to the gpu partition
+    2) Lower the number of nodes in the other partitions until utilization balances out
+'''
